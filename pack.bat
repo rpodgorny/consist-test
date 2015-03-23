c8d759015c6eb478e93751c105a1b351eee928a6
Radek Podgorny <radek@podgorny.cz>
2015-03-23 15:24:40 +0100

    added loader file and improved pack scripts

--- pack.bat
+++ pack.bat
@@ -3,6 +3,7 @@ echo this is compile.bat v0.3
 setlocal
 
 set name=consist
+set pkgrel=2
 
 rd /s /q pkg
 md pkg
@@ -11,6 +12,8 @@ cp -av consistometer.exe pkg/atx300/%name%/
 cp -av consistometer.ini pkg/atx300/%name%/
 ;rem md pkg\atx300\lingual\data\cs
 ;rem cp -av control.cs pkg/atx300/lingual/data/cs/
+md pkg\atx300\loader\data
+cp -av consist.loader pkg/atx300/loader/data/consist
 cp atxpkg_backup pkg/.atxpkg_backup
 
 hg parents --template "{latesttag}" >.version
@@ -28,14 +31,16 @@ if "%1" == "" (
 
 	set name=%name%.dev
 	set version=%datetime%
+	set upload=atxpkg@atxpkg-dev.asterix.cz:atxpkg/
 ) else if "%1" == "release" (
 	echo release version %version%
+	set upload=atxpkg@atxpkg.asterix.cz:atxpkg/
 ) else (
 	echo unknown parameter!
 	goto end
 )
 
-set pkg_fn=%name%-%version%.atxpkg.zip
+set pkg_fn=%name%-%version%-%pkgrel%.atxpkg.zip
 
 rm %pkg_fn%
 
@@ -45,6 +50,6 @@ cd ..
 
 rd /s /q pkg
 
-pscp %pkg_fn% radek@podgorny.cz:public_html/atxpkg/
+pscp %pkg_fn% %upload%
 
 :end
